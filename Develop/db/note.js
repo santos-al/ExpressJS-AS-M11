ce9224153d807485b573081e5edb48ccbd529bca
Xandi <santosal2018@gmail.com>
2023-08-02 21:13:12 -0400

    add a read note and write note for note class

---
+++ Develop/db/note.js
@@ -0,0 +1,19 @@
+// Used to read and write files
+const util = require('util');
+const fs = require('fs');
+
+// Used to generate a unique id for each note
+const uuid = require('../helpers/uuid.js');
+const { json } = require('express');
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Note {
+    readNote() {
+        return readFileAsync('db.json');
+    }
+    writeNote() {
+        return writeFileAsync('db.json', json.stringify(note));
+    }
+}
