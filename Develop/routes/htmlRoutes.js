cddce6047de95a8cb69bdd71245e5eec59fc9863
Xandi <santosal2018@gmail.com>
2023-08-02 20:18:58 -0400

    add html route comments

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const router = require('express').Router();
 
+// sets route /notes to notes.html
 router.get('/notes', (re, res) => {
     res.sendFile(path.join(__dirname, '../notes.html'));
 });
 
+// sets any other route to index.html
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'));
 });
